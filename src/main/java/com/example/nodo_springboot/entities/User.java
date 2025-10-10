adfb5d8561f5f657d7ea3852a344be2e992d273a
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-10-10 15:41:22 +0700

    Initial commit

---
+++ src/main/java/com/example/nodo_springboot/entities/User.java
@@ -0,0 +1,57 @@
+package com.example.nodo_springboot.entities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    @Autowired
+    private Address address;
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, Address address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
